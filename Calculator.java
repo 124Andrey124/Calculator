7ff79f84ceba4640da84886c1003fda6ac7835da
124Andrey124 <125967151+124Andrey124@users.noreply.github.com>
2023-02-27 15:47:30 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package Calculator;
+
+import java.util.InputMismatchException;
+
+public class Calculator {
+
+    public int calculated(int num1, int num2, char operation) {
+        int res = 0;
+        switch (operation) {
+            case '+':
+                res = num1 + num2;
+                break;
+            case '-':
+                res = num1 - num2;
+                break;
+            case '*':
+                res = num1 * num2;
+                break;
+            case '/':
+                res = num1 / num2;
+                break;
+        }
+        return res;
+    }
+
+}
